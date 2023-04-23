a303f563af112c0c15e053c09ce3949735df5d55
Fabrice-Etienne <fabricecode01@gmail.com>
2023-04-23 15:34:28 +0200

    Fin du CSS adapté aux écrans (min-width: 320px) avec un darck & lighte mode

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ darkMode.addEventListener('click', () =>{
     darkMode.setAttribute('style', 'display: none ;') ;
     lightMode.setAttribute('style', 'display: inline ;') ;
     body.setAttribute('style', 'background: #ECE9E6 ; background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6); background: linear-gradient(to right, #FFFFFF, #ECE9E6);  color: #030303 ;') ;
-    myName.style.color = '#030303' ;
+    myName.style.color = 'var(--black-color)' ;
 }) ;
 
 // Light mode => Dark mode 
@@ -16,4 +16,5 @@ lightMode.addEventListener('click', () =>{
     lightMode.setAttribute('style', 'display: none ;') ;
     darkMode.setAttribute('style', 'display: inline ;') ;
     body.setAttribute('style', 'background: #141E30; background: -webkit-linear-gradient(to right, #141E30, #243B55); background: linear-gradient(to right, #141E30, #243B55);') ;
+    myName.style.color = 'var(--text-color)' ;
 }) ;
