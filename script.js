13134a3402061d11098f9571dbed964270c506a9
Fabrice-Etienne <fabricecode01@gmail.com>
2023-04-23 14:29:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Dark & light mode
+const darkMode  = document.getElementById('dark-mode') ;
+const lightMode = document.getElementById('light-mode') ;
+const body      = document.querySelector('body') ;
+const myName    = document.getElementById('my-name') ;
+// Dark mode => Light mode 
+darkMode.addEventListener('click', () =>{
+    darkMode.setAttribute('style', 'display: none ;') ;
+    lightMode.setAttribute('style', 'display: inline ;') ;
+    body.setAttribute('style', 'background: #ECE9E6 ; background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6); background: linear-gradient(to right, #FFFFFF, #ECE9E6);  color: #030303 ;') ;
+    myName.style.color = '#030303' ;
+}) ;
+
+// Light mode => Dark mode 
+lightMode.addEventListener('click', () =>{
+    lightMode.setAttribute('style', 'display: none ;') ;
+    darkMode.setAttribute('style', 'display: inline ;') ;
+    body.setAttribute('style', 'background: #141E30; background: -webkit-linear-gradient(to right, #141E30, #243B55); background: linear-gradient(to right, #141E30, #243B55);') ;
+}) ;
