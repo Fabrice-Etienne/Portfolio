543f9eed285f1b8cb790ac444cea60314dc3d69e
Fabrice-Etienne <fabricecode01@gmail.com>
2023-05-01 02:58:08 +0200

    End of css for the first page (index.html)

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
+const body      = document.querySelector('body') ;
+const myName    = document.getElementById('my-name') ;
+
 // Dark & light mode
 const darkMode  = document.getElementById('dark-mode') ;
 const lightMode = document.getElementById('light-mode') ;
-const body      = document.querySelector('body') ;
-const myName    = document.getElementById('my-name') ;
-const userName = document.getElementById('user-name') ;
+const navLinks  = document.querySelectorAll('.nav-links') ;
+
+// the color of nav links depends of the page the user
+let userwebPage = window.location.pathname.replace("/", "") ;
+
+// add a css class for the link of the current page
+switch (userwebPage) {
+    case 'about.html':
+        document.querySelector('a[href="./about.html"]').classList.add('active-link');
+        break;
+    case 'projects.html':
+        document.querySelector('a[href="./projects.html"]').classList.add('active-link');
+        break;
+    case 'contact.html':
+        document.querySelector('a[href="./contact.html"]').classList.add('active-link');
+        break;
+    default:
+        document.querySelector('a[href="./index.html"]').classList.add('active-link');
+}
+
 // Dark mode => Light mode 
 darkMode.addEventListener('click', () =>{
     darkMode.setAttribute('style', 'display: none ;') ;
-    lightMode.setAttribute('style', 'display: inline ;') ;
+    lightMode.setAttribute('style', 'display: inline-block ;') ;
     body.setAttribute('style', 'background: #ECE9E6 ; background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6); background: linear-gradient(to right, #FFFFFF, #ECE9E6);  color: #030303 ;') ;
+    // change the color of my name 
     myName.style.color = 'var(--black-color)' ;
+    // create a loop to change the color of each of the navigation links 
+    navLinks.forEach( (navLink) => {
+        navLink.style.color = 'var(--black-color)' ;
+    });
 }) ;
 
 // Light mode => Dark mode 
 lightMode.addEventListener('click', () =>{
     lightMode.setAttribute('style', 'display: none ;') ;
-    darkMode.setAttribute('style', 'display: inline ;') ;
+    darkMode.setAttribute('style', 'display: inline-block ;') ;
     body.setAttribute('style', 'background: #141E30; background: -webkit-linear-gradient(to right, #141E30, #243B55); background: linear-gradient(to right, #141E30, #243B55);') ;
     myName.style.color = 'var(--text-color)' ;
+    // create a loop to change the color of each of the navigation links 
+    navLinks.forEach( (navLink) => {
+        navLink.style.color = 'var(--text-color)' ;
+    });
 }) ;
 
 // Slider menu
